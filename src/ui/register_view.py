d7d6b5ddbb9596516fae613d5d94a9edff3fa1a2
niilolehtonen <niilo.i.lehtonen@gmail.com>
2023-04-04 21:50:47 +0300

    added password fields

--- src/ui/register_view.py
+++ src/ui/register_view.py
@@ -18,12 +18,22 @@ class RegisterView:
         username_label = ttk.Label(master=self._frame, text="Username:")
         username_entry = ttk.Entry(master=self._frame, text="")
 
+        password1_label = ttk.Label(master=self._frame,text="Password:")
+        password1_entry = ttk.Entry(master=self._frame,text="",show="*")
+        password2_label = ttk.Label(master=self._frame,text="Password again:")
+        password2_entry = ttk.Entry(master=self._frame,text="",show="*")
+
         button_register = ttk.Button(master=self._frame,text="Register")
 
 
         label1.grid(row=0, column=0)
         username_label.grid(row=2, column=0)
         username_entry.grid(row=2, column=1)
-        button_register.grid(row=3, column=0)
+        password1_label.grid(row=3, column=0)
+        password1_entry.grid(row=3, column=1)
+        password2_label.grid(row=4, column=0)
+        password2_entry.grid(row=4, column=1)
+        button_register.grid(row=5, column=0)
+
     def pack(self):
         self._frame.pack(fill=constants.X)
