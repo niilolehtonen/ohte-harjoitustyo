6e02bd191cb801bff0343debfc956f57ee6a1088
niilolehtonen <niilo.i.lehtonen@gmail.com>
2023-04-13 17:41:18 +0300

    login wip

--- src/ui/login_view.py
+++ src/ui/login_view.py
@@ -1,37 +1,64 @@
 from tkinter import ttk, constants, StringVar
+import os, sys
+dir = os.path.dirname("budget_service.py")
+sys.path.append(dir)
+from src.services.budget_service import budget_service, InvalidCredentialsError
 
 class LoginView:
-    def __init__(self, root):
+    def __init__(self, root, handle_login, handle_show_RegisterView):
         self._root = root
         self._frame = None
+        self._handle_show_create_user_view = handle_show_RegisterView
+        self._handle_login = handle_login
 
         self._initialize()
 
     def destroy(self):
         self._frame.destroy()
 
+
+    def _login_handler(self):
+        username = self._username_entry.get()
+        password = self._password_entry.get()
+
+        try:
+            budget_service.login(username, password)
+            self._handle_login()
+        except InvalidCredentialsError:
+            self._show_error("Invalid username or password")
+
+    def _initialize_username_field(self):
+        username_label = ttk.Label(master=self._frame, text="Username")
+
+        self._username_entry = ttk.Entry(master=self._frame)
+
+        username_label.grid(padx=5, pady=5, sticky=constants.W)
+        self._username_entry.grid(padx=5, pady=5, sticky=constants.EW)
+
+    def _initialize_password_field(self):
+        password_label = ttk.Label(master=self._frame, text="Password")
+
+        self._password_entry = ttk.Entry(master=self._frame, show="*")
+
+        password_label.grid(padx=5, pady=5, sticky=constants.W)
+        self._password_entry.grid(padx=5, pady=5, sticky=constants.EW)
+
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
         label1 = ttk.Label(master=self._frame, text="Login")
         
-        username_var = StringVar()
-        username_label = ttk.Label(master=self._frame, text="Username:")
-        username_entry = ttk.Entry(master=self._frame, text="")
 
-        password1_label = ttk.Label(master=self._frame,text="Password:")
-        password1_entry = ttk.Entry(master=self._frame,text="",show="*")
+        button_login = ttk.Button(master=self._frame,text="Login",command=self._login_handler)
+        button_create= ttk.Button(master=self._frame,text="Create an account",command=self._handle_show_create_user_view)
 
-        button_login = ttk.Button(master=self._frame,text="Login")
-        button_create= ttk.Button(master=self._frame,text="Create an account")
-
+        self._initialize_username_field()
+        self._initialize_password_field()
 
         label1.grid(row=0, column=0)
-        username_label.grid(row=2, column=0)
-        username_entry.grid(row=2, column=1)
-        password1_label.grid(row=3, column=0)
-        password1_entry.grid(row=3, column=1)
-        button_login.grid(row=5, column=0)
-        button_create.grid(row=5, column=1)
 
+        button_login.grid(padx=5, pady=5, sticky=constants.EW)
+        button_create.grid(padx=5, pady=5, sticky=constants.EW)
+
+        self._frame.grid_columnconfigure(0, weight=1, minsize=400)
     def pack(self):
         self._frame.pack(fill=constants.X)
