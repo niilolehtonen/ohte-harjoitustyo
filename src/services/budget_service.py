29337665fab4879b9cf1fe8e071f369b9754c4af
niilolehtonen <niilo.i.lehtonen@gmail.com>
2023-04-13 17:38:24 +0300

    create user & login functions

--- src/services/budget_service.py
+++ src/services/budget_service.py
@@ -1,9 +1,31 @@
-from user import User
-from repositories.user_repository import (user_repository as default_user_repository)
+from src.user import User
+from src.repositories.user_repository import (user_repository as default_user_repository)
 
+class InvalidCredentialsError(Exception):
+    pass
 
 class BudgetService:
 
     def __init__(self,user_repository=default_user_repository):
         self._user = None
         self._user_repository = user_repository
+
+    def create_user(self, username, password):
+
+        user = self._user_repository.create_user(User(username, password))
+
+        return user
+
+    def login(self,username,password):
+        
+        user = self._user_repository.search_username(username)
+
+        if not user or user.password != password:
+            raise InvalidCredentialsError("Invalid username or password")
+
+        self._user = user
+
+        return user
+
+
+budget_service = BudgetService()
