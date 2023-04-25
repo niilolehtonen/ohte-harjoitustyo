865676544c1029cca8208c03c5f437abbd3fcf87
niilolehtonen <niilo.i.lehtonen@gmail.com>
2023-04-25 15:08:32 +0300

    more logix

--- src/services/budget_service.py
+++ src/services/budget_service.py
@@ -1,14 +1,17 @@
 from src.user import User
+from src.transaction import Transaction
 from src.repositories.user_repository import (user_repository as default_user_repository)
+from src.repositories.transaction_repository import (transaction_repository as default_transaction_repository)
 
 class InvalidCredentialsError(Exception):
     pass
 
 class BudgetService:
 
-    def __init__(self,user_repository=default_user_repository):
+    def __init__(self,user_repository=default_user_repository,transaction_repository=default_transaction_repository):
         self._user = None
         self._user_repository = user_repository
+        self._transaction_repository = transaction_repository
 
     def create_user(self, username, password):
 
@@ -26,5 +29,46 @@ class BudgetService:
         self._user = user
 
         return user
+    
+    def new_expense(self,name,amount):
 
+        u_id = self._transaction_repository.get_user_id(self._user)
+        type = 'expense'
+        expense = Transaction(u_id,type,name,amount)
+        self._transaction_repository.new_transcation(expense)
+
+    def new_income(self,name,amount):
+
+        u_id = self._transaction_repository.get_user_id(self._user)
+        type = 'income'
+        income = Transaction(u_id,type,name,amount)
+        self._transaction_repository.new_transcation(income)
+
+    def show_budget(self):
+        expenses = []
+        incomes = []
+        sum_expenses = 0
+        sum_incomes = 0
+        transactions = self._transaction_repository.get_transactions(self._user)
+        if transactions:
+            for i in transactions:
+                if i[1] == 'expense':
+                    expenses.append(i)
+                if i[1] == 'income':
+                    incomes.append(i)
+        
+        if len(expenses) > 0:
+            for i in expenses:
+                sum_expenses += i[3]
+
+        if len(incomes) > 0:
+            for i in incomes:
+                sum_incomes += i[3]
+
+        budget = sum_incomes - sum_expenses
+
+        return (budget,expenses,incomes)
+
+        
+        
 budget_service = BudgetService()
