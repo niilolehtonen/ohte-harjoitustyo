50f30d662df13ceaa909e7f6b944d70c6609ca04
niilolehtonen <102043449+niilolehtonen@users.noreply.github.com>
2023-05-02 16:17:08 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -3,7 +3,6 @@ from invoke import task
 @task
 def start(ctx):
     ctx.run("python3 src/ui/ui.py", pty=True)
-    # At the moment this only runs the ui.py file because i haven't created 'index.py'
 
 @task
 def test(ctx):
